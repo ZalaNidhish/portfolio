f81de50b4e9abc4e00fea6afae4dc6208119aab6
Nidhish Zala <nidhish1132007@gmail.com>
2026-03-30 17:40:29 +0530

    added pricing page

---
+++ Frontend/src/components/ProjectSlider.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ProjectSlider = () => {
+  return (
+    <div className='min-h-screen w-screen bg-neutral-700'>
+        <div>
+            
+        </div>
+    </div>
+  )
+}
+
+export default ProjectSlider
